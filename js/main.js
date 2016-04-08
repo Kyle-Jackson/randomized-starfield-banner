ed5b08d24ad9104831cba81a4834529777c575d6
Kyle-Jackson <kylejackson@Kyles-MacBook-Pro.local>
2016-04-07 22:14:53 -0400

    Added js and tried to animate the stars.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 var $body = $('.banner');
-var $stars;
+var $star;
+var bannerWidth = $body.outerWidth();
+var bannerHeight = $body.outerHeight();
+
+for (i = 0; i < 75; i++) {
+  $star = $('<div>');
+  $star.addClass('star')
+  $star.css({
+    left: Math.random() * bannerWidth,
+    top: Math.random() * bannerHeight,
+    transform: 'rotate(' + Math.random() * 360 + 'deg) scale(' + Math.random() + ')',
+    opacity: Math.random()
+  });
+  $body.append($star);
+}
